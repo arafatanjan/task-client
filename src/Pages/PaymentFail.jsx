5c055ade622d0e27f44d26698f4ceb74297ae7df
arafatanjan <arafatanjan@gmail.com>
2024-09-20 17:32:14 +0600

    first commit

--- src/Pages/PaymentFail.jsx
+++ src/Pages/PaymentFail.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import pic from '../assets/fail.png'
 
 const PaymentFail = () => {
+    // const {tranId} = useParams();
     return (
         <div>
-            <h1>Payment failed</h1>
+           <h2>Payment Fail </h2>
+           <div className="d-flex justify-contect-center mt-3 mb-3">
+            <img
+              src={pic}
+              alt="fail"
+            />
+          </div>
+           {/* <h3>{tranId}</h3> */}
         </div>
     );
 };
--- src/Pages/PaymentSuccess.jsx
+++ src/Pages/PaymentSuccess.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import pic from '../assets/successful.png'
 
 const PaymentSuccess = () => {
     const {tranId} = useParams();
     return (
         <div>
            <h2>Payment Successful </h2>
+           <div className="d-flex justify-contect-center mt-3 mb-3">
+            <img
+              src={pic}
+              alt="successful"
+            />
+          </div>
            <h3>{tranId}</h3>
         </div>
     );
